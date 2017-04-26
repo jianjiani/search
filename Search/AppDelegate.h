61451039fe6abd7734c3c74b9ad5bee5c8fe2347
jianjian1019 <yingjian_sun@163.com>
2017-04-26 14:15:17 +0800

    Initial Commit

---
+++ Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Search
+//
+//  Created by 孙英建 on 2017/4/26.
+//  Copyright © 2017年 孙英建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Search/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Search
+//
+//  Created by 孙英建 on 2017/4/26.
+//  Copyright © 2017年 孙英建. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
